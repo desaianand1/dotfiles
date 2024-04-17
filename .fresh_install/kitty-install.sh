39310982cde4358798c4f956e3fdca46555612d6
Anand Desai <desaianand456@gmail.com>
2024-04-16 22:15:38 -0400

    feat: changed kitty theme

--- .fresh_install/kitty-install.sh
+++ .fresh_install/kitty-install.sh
@@ -4,4 +4,8 @@
 # See installation instructions here in case they changed: https://sw.kovidgoyal.net/kitty/binary/
 echo "Installing kitty..."
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+echo "Installing kitty themes..."
+# Install all the kitty themes available
+# Use `kitten themes` to interactively select one (modifies kitty.conf)
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
 echo "✨Done! kitty installed!"
