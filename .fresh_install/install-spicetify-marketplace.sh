d23205251ae334a78f8328085700b61bbfb1a482
Anand Desai <desaianand456@gmail.com>
2024-09-20 16:53:57 +0530

    feat: renamed old scripts

--- .fresh_install/install-spicetify-marketplace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install Marketplace extension for Spicetify
-echo "Installing Spicetify Marketplace..."
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
-echo "✨Done! Spicetify Marketplace installed!"
